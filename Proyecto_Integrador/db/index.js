73c331cc60080e8f028444650da78aa0a8eab6c4
si390 <soyolaiberico@udesa.edu.ar>
2024-04-07 23:09:37 -0300

    db

---
+++ Proyecto_Integrador/db/index.js
@@ -0,0 +1,74 @@
+const datos = {
+    usuario: [
+        {
+            usuario: "sdasd",
+            email: "email@gmail.com",
+            constraseña: 'sd15152',
+            documento: 25896322,
+            fecha_nacimiento: "02/5/1990",
+            foto_perfil: "/images/users/default-image.png",
+        },
+   ],
+    productos: [
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            imagen: "images/products/default-image.png",
+            nombre: "celular",
+            descripcion: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        },
+        {
+            comentarios: [
+                {
+                    nombreUsuario: "smaknsdk",
+                    textoComentario: "sjdjiasalaL",
+                    imagenPerfil: "/images/users/default-image.png"
+                }
+            ]
+        },
+   ]
+}
+module.exports = datos
