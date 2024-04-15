aeda73630144b125091ff3e7c7cd46ea0d83ca7a
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-04-15 16:24:40 -0300

    Subo view de perfil

--- Proyecto_Integrador/controllers/profileController.js
+++ Proyecto_Integrador/controllers/profileController.js
@@ -4,13 +4,13 @@ const datos = usuario.perfil;
 const profileController = {
 
     nombre: function (req, res){
-        return res.render('profile', datos.nombre);
+        return res.render('profile', {nombre: datos.nombre});
     },
 
     email: function (req, res){
-        return res.render('profile', datos.email);
+        return res.render('profile', {email: datos.email});
     },
-
+    
 }
 
 module.exports = profileController;
