5e3befe29058a62e691e950bac901e1b783f58b7
si390 <soyolaiberico@udesa.edu.ar>
2024-04-15 15:58:23 -0300

    values tabla

--- Proyecto_Integrador/routes/products.js
+++ Proyecto_Integrador/routes/products.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const productController = require('../controllers/productController');
-const registroController = require('../controllers/registroController');
-const addproductController = require("../controllers/addproductController");
+
 
 //página descripción de producto//
 router.get('/', productController.usuario);
