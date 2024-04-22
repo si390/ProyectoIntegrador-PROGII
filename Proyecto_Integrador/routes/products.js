76b943af49739c3d5da2cef965491788e000ba77
si390 <soyolaiberico@udesa.edu.ar>
2024-04-22 15:33:23 -0300

    productcontroller

--- Proyecto_Integrador/routes/products.js
+++ Proyecto_Integrador/routes/products.js
@@ -4,11 +4,11 @@ var router = express.Router();
 const productController = require('../controllers/Controller');
 
 //página descripción de producto//
-router.get('/', productController.producto);
+router.get('/nombre', productController.producto);
 
-router.get('/descripcion', productController.descripcion);
+router.get('/nombre/descripcion', productController.descripcion);
 
-router.get('/descripcion', productController.comentarios)
+router.get('/nombre/comentarios', productController.comentarios)
 
 module.exports = router;
 
