3a5291999079ef0aa7884b891838b0fefbfd8d06
si390 <soyolaiberico@udesa.edu.ar>
2024-04-22 17:12:45 -0300

    fix

--- Proyecto_Integrador/controllers/Controller.js
+++ Proyecto_Integrador/controllers/Controller.js
@@ -1,22 +1,22 @@
 const datos = require('../db/index')
 
 const Controller = {
-    mostrarProducto : function(res, res){
+    mostrarProducto : function(req, res){
         const producto = datos.productos.lista[0];
         return res.render('product', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcion: producto.descripcion, comentarios: producto.comentarios, usuario: producto.comentarios.usuario })
     },
     
-    index: function(res, res){
+    index: function(req, res){
         const producto = datos.productos.lista[0];
         
-        const descripcioncorta = '';
+        let descripcioncorta = '';
         for (let i = 0; i < producto.descripcion.length; i++) {
-        descripcionCorta += producto.descripcion[i];
-        if (producto.descripcion[i] === '.') {
-        break;
+        descripcioncorta += producto.descripcion[i];
+            if (producto.descripcion[i] === '.') {
+                break;
     }
-        return res.render('index', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcioncorta: descripcioncorta})
+        
+    }return res.render('index', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcioncorta: descripcioncorta})
 
-    }
 }}
 module.exports = Controller;
--- Proyecto_Integrador/routes/products.js
+++ Proyecto_Integrador/routes/products.js
@@ -3,16 +3,13 @@ var router = express.Router();
 
 const productController = require('../controllers/Controller');
 
-//Index//
-router.get('/index', productController.producto);
-router.get('/index', productController.fotoproducto);
-router.get('/index/descripcion', productController.descripcion);
-
 //página descripción de producto//
-router.get('/products', productController.producto);
-router.get('/products', productController.fotoproducto);
-router.get('/products/descripcion', productController.descripcion);
-router.get('/products', productController.comentarios)
+router.get('/', productController.mostrarProducto);
+//Index//
+router.get('/index', productController.index);
+
+
+
 
 
 
