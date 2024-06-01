b5febb45afbc3692892618cc71c2b4eb9bea9b0b
si390 <soyolaiberico@udesa.edu.ar>
2024-06-01 20:34:24 -0300

    arreglo index db

--- Proyecto_Integrador/controllers/Controller.js
+++ Proyecto_Integrador/controllers/Controller.js
@@ -1,49 +1,69 @@
-const datos = require('../db/index')
-const db = require('../database/models')
+const datos = require('../db/index');
+const db = require('../database/models');
 
 const Controller = {
-
-   mostrarIndex: function(req,res) {
-    const productos = datos.productos.lista;
+    mostrarIndex: function(req, res) {
+        const productos = datos.productos.lista;
         const masComentados = [];
         const novedades = [];
 
-        for (let i = 0; i < productos.length; i++) {
-            if (productos[i].comentarios.length > 4) {
-                masComentados.push(productos[i]);
+        productos.forEach(producto => {
+            if (producto.comentarios.length > 4) {
+                masComentados.push(producto);
             } else {
-                novedades.push(productos[i]);
+                novedades.push(producto);
             }
+        });
+
+        return res.render('index', { masComentados: masComentados, novedades: novedades });
+    },
+
+    mostrarProducto: function(req, res) {
+        const productId = req.params.id;
+        const producto = datos.productos.lista.find(p => p.id == productId);
+
+        if (!producto) {
+            return res.status(404).send('El producto no se encuentra disponible');
         }
-        return res.render('index', {masComentados: masComentados, novedades: novedades});
-   },
 
-    mostrarProducto : function(req, res){
-        const producto = datos.productos.lista[0];
-        return res.render('product', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcion: producto.descripcion, comentarios: producto.comentarios, usuario: producto.comentarios.usuario })
+        return res.render('product', {
+            nombre: producto.nombre,
+            fotoproducto: producto.imagen,
+            descripcion: producto.descripcion,
+            comentarios: producto.comentarios,
+            usuario: producto.comentarios.map(c => c.usuario)
+        });
     },
 
-    search: function(req, res){
-        const producto = datos.productos.lista[0];
-        return res.render('search-results', {nombre: producto.nombre, descripcion: producto.descripcion, comentarios: producto.comentarios});
+    search: function(req, res) {
+        const query = req.query.q;
+        const resultados = datos.productos.lista.filter(p => 
+            p.nombre.includes(query) || p.descripcion.includes(query)
+        );
+
+        return res.render('search-results', { resultados: resultados });
     },
 
-    informacion : function(req, res){
-        const producto = datos.productos.lista[0];
-        return res.render('product', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcion: producto.descripcion, comentarios: producto.comentarios, usuario: producto.comentarios.usuario })
+    informacion: function(req, res) {
+        return this.mostrarProducto(req, res);
     },
-    
-    detalle: function(req, res){
-        const producto = datos.productos.lista[0];
-        
-        let descripcioncorta = '';
-        for (let i = 0; i < producto.descripcion.length; i++) {
-        descripcioncorta += producto.descripcion[i];
-            if (producto.descripcion[i] === '.') {
-                break;
+
+    detalle: function(req, res) {
+        const productId = req.params.id;
+        const producto = datos.productos.lista.find(p => p.id == productId);
+
+        if (!producto) {
+            return res.status(404).send('El producto no se encuentra disponible');
+        }
+
+        let descripcioncorta = producto.descripcion.split('.').slice(0, 2).join('.') + '.';
+
+        return res.render('product-add', {
+            nombre: producto.nombre,
+            fotoproducto: producto.imagen,
+            descripcioncorta: descripcioncorta
+        });
     }
-        
-    }return res.render('product-add', {nombre: producto.nombre, fotoproducto: producto.imagen, descripcioncorta: descripcioncorta})}
 }
 
 module.exports = Controller;
--- Proyecto_Integrador/db/index.js
+++ Proyecto_Integrador/db/index.js
@@ -1,7 +1,6 @@
-const basedatos = {
-
-    usuarios: {
-        lista: [{
+const db = {
+    usuarios: [
+        {
             nombre: "Juan",
             contraseña: "lock100",
             fechaNacimiento: "1970-12-12",
@@ -41,236 +40,260 @@ const basedatos = {
             fotoPerfil: "/images/users/default-image.png",
             dni: 52102580,
         },
-        ]
-    },
-
-    productos: {
-        lista: [
-            {
-                id: 1,
-                nombre: "Buzo Antisocial",
-                descripcion: "¡Un buzo diseñado a tu medida!. Elegí el color que más te guste.",
-                imagen: "/images/products/buzoantisocial.jpg",
-                comentarios: [
-                    {
-                        usuario: "Juan",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "default-image.png"
-                    },
-                    {
-                        usuario: "Maria",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "default-image.png"
-                    },
-                ]
-            },
-            {
-                id: 2,
-                nombre: "Buzo AreoWorld",
-                descripcion: "Un buzo para todos",
-                imagen: "/images/products/buzoastroworld.jpg",
-                comentarios: [
-                    {
-                        usuario: "Pedro",
-                        mensaje: "No me gustó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-            {
-                id: 3,
-                nombre: "Buzo Future",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzofuture.jpg",
-                comentarios: [
-                    {
-                        usuario: "Usuario",
-                        mensaje: "Buenísimo!",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-            {
-                id: 4,
-                nombre: "Buzo Butterfly",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzobutterfly.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    },
-                    {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-            {
-                id: 5,
-                nombre: "Buzo Car",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzocar.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    },]
-            },
-            {
-                id: 6,
-                nombre: "Buzo Chicago",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzochicago.jpg",
-                comentarios: [
-                    {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-            {
-                id: 7,
-                nombre: "Buzo Eminem",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzoeminem.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "¿Tienen en talle m?",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    },]
-            },
-            {
-                id: 8,
-                nombre: "Buzo King Of The Kongo",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzokingofthekongo.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "default-image.png"
-                    },]
-            },
-            {
-                id: 9,
-                nombre: "Buzo Los Angeles",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzolosangeles.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-            {
-                id: 10,
-                nombre: "Buzo Red",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzored.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    },]
-            },
-            {
-                id: 11,
-                nombre: "Buzo Tupac",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzotupac.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    },]
-            },
-            {
-                id: 12,
-                nombre: "Buzo Undefined",
-                descripcion: "Descripción",
-                imagen: "/images/products/buzoundefined.jpg",
-                comentarios: [
-                    {
-                        usuario: "usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "./maqueta/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    }, {
-                        usuario: "Usuario",
-                        mensaje: "Me encantó el producto",
-                        fotoPerfil: "/images/users/default-image.png"
-                    },]
-            },
-        ]
-    }
+    ],
+    productos: [
+        {
+            id: 1,
+            nombre: "Buzo Antisocial",
+            descripcion: "¡Un buzo diseñado a tu medida!. Elegí el color que más te guste.",
+            imagen: "/images/products/buzoantisocial.jpg",
+            comentarios: [
+                {
+                    usuario: "Juan",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "default-image.png",
+                },
+                {
+                    usuario: "Maria",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "default-image.png",
+                },
+            ],
+        },
+        {
+            id: 2,
+            nombre: "Buzo AreoWorld",
+            descripcion: "Un buzo para todos",
+            imagen: "/images/products/buzoastroworld.jpg",
+            comentarios: [
+                {
+                    usuario: "Pedro",
+                    mensaje: "No me gustó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 3,
+            nombre: "Buzo Future",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzofuture.jpg",
+            comentarios: [
+                {
+                    usuario: "Usuario",
+                    mensaje: "Buenísimo!",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 4,
+            nombre: "Buzo Butterfly",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzobutterfly.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 5,
+            nombre: "Buzo Car",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzocar.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 6,
+            nombre: "Buzo Chicago",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzochicago.jpg",
+            comentarios: [
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 7,
+            nombre: "Buzo Eminem",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzoeminem.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "¿Tienen en talle m?",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 8,
+            nombre: "Buzo King Of The Kongo",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzokingofthekongo.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "default-image.png",
+                },
+            ],
+        },
+        {
+            id: 9,
+            nombre: "Buzo Los Angeles",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzolosangeles.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 10,
+            nombre: "Buzo Red",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzored.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 11,
+            nombre: "Buzo Tupac",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzotupac.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+            ],
+        },
+        {
+            id: 12,
+            nombre: "Buzo Undefined",
+            descripcion: "Descripción",
+            imagen: "/images/products/buzoundefined.jpg",
+            comentarios: [
+                {
+                    usuario: "usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "./maqueta/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+                {
+                    usuario: "Usuario",
+                    mensaje: "Me encantó el producto",
+                    fotoPerfil: "/images/users/default-image.png",
+                },
+            ],
+        },
+    ],
 };
 
-module.exports = basedatos;
+module.exports = db;
