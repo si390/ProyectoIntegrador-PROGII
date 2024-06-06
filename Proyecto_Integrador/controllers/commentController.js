c43fc09146c6e84afbbc3001d022d31acea011e6
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-06-06 00:08:01 -0300

    creo funciones de comentarios

--- Proyecto_Integrador/controllers/commentController.js
+++ Proyecto_Integrador/controllers/commentController.js
@@ -2,6 +2,22 @@ const db = require('../database/models');
 
 const commentController = {
 
+    crearComentario: function (req, res) {
+        
+    },
+
+    mostrarComentarios: function (req, res) {
+        db.Product.findAll({
+            order: [['createdAt', 'DESC']]
+        })
+        .then(function(comentarios){
+
+        })
+        .catch(function(error){
+            return res.render("product", { error: "error al mostrar comentarios" });
+        })
+    },
+
 };
 
 module.exports = commentController;
