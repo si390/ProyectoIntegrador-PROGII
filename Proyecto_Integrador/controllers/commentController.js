f9cd51ff6f948655101a68a2a4580434ca54279b
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-06-23 20:04:20 -0300

    cambio comentarios

--- Proyecto_Integrador/controllers/commentController.js
+++ Proyecto_Integrador/controllers/commentController.js
@@ -3,33 +3,33 @@ const { validationResult } = require("express-validator");
 
 const commentController = {
 
-    crearComentario: function (req, res) {
-        const errors = validationResult(req);
-        const productId = req.params.productoId;
+        crearComentario: function (req, res) {
+            const errors = validationResult(req);
+            const productId = req.params.productId; 
+    
+            if (!req.session.user) {
+                const error = "Debes estar logueado para comentar";
+                return res.render('product', { producto: { id: productId }, loguearse: error });
+            }
+            const texto = req.body.comment;
+    
+            if (errors.isEmpty()) {
+                db.Comentario.create({
+                    texto,
+                    productoId: productId,
+                    usuarioId: req.session.user.id,
+                })
+                .then(() => {
+                    res.redirect(`/product/${productId}`);
+                })
+                .catch((error) => {
 
-        if (!req.session.user) {
-            const error = "Debes estar logueado para comentar";
-            return res.render('product/detail', { productId, loguearse: error });
+                    res.render('product', { producto: { id: productId }, error: "Error al subir comentario" });
+                });
+            } else {
+                res.render('product', { producto: { id: productId }, errors: errors.mapped(), old: req.body });
+            }
         }
-        const texto = req.body.comment;
-
-        if (errors.isEmpty()) {
-            db.Comentario.create({
-                texto,
-                productoId: productId,
-                usuarioId: req.session.user.id,
-            })
-            .then(() => {
-                res.redirect(`/product/${productId}`);
-            })
-            .catch((error) => {
-                console.error("Error al crear comentario:", error);
-                res.render('product/detail', { productId, error: "Error al subir comentario" });
-            });
-        } else {
-            res.render('product/detail', { productId, errors: errors.mapped(), old: req.body });
-        }
-    }
 };
 
 module.exports = commentController;
