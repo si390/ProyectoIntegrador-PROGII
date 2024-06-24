50518ed76631ac0ba8d12a1037e6dc8a361ad7f0
si390 <soyolaiberico@udesa.edu.ar>
2024-06-23 23:03:39 -0300

    comentariosintoagregar

--- Proyecto_Integrador/controllers/commentController.js
+++ Proyecto_Integrador/controllers/commentController.js
@@ -1,37 +1,44 @@
 const db = require('../database/models');
 const { validationResult } = require("express-validator");
-const Product = db.Product;
-const Usuario = db.Usuario;
-const Comentario = db.Comentario;
+
+
 const commentController = {
 
-        crearComentario: function (req, res) {
-            const errors = validationResult(req);
-            const productId = req.params.productId; 
-    
-            if (!req.session.user) {
-                const error = "Debes estar logueado para comentar";
-                return res.render('product', { producto: { id: productId }, loguearse: error });
-            }
-            const texto = req.body.comment;
-    
-            if (errors.isEmpty()) {
-                db.Comentario.create({
-                    texto,
-                    productoId: productId,
-                    usuarioId: req.session.user.id,
-                })
-                .then(() => {
-                    res.redirect(`/product/${productId}`);
-                })
-                .catch((error) => {
-
-                    res.render('product', { producto: { id: productId }, error: "Error al subir comentario" });
-                });
-            } else {
-                res.render('product', { producto: { id: productId }, errors: errors.mapped(), old: req.body });
-            }
+    crearComentario: function(req, res) {
+        if (!req.session.user) {
+            return res.redirect('/profile/login');
         }
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return db.Producto.findByPk(req.params.productoId, {
+                include: [{ model: db.Comentario, include: [db.Usuario] }]
+            })
+            .then(producto => {
+                res.render('product', {
+                    producto: producto,
+                    comentarios: producto.Comentarios,
+                    errors: errors.mapped()
+                });
+            });
+        }
+
+        const { comment } = req.body;
+        db.Comentario.create({
+            texto: comment,
+            usuario_id: req.session.user.id,
+            producto_id: req.params.productoId,
+            created_at: new Date()
+        })
+        .then(() => {
+            res.redirect(`/product/${req.params.productoId}`);
+        })
+        .catch(error => {
+            res.status(500).send(error.message);
+        });
+    },
+    
 };
+
 
 module.exports = commentController;
