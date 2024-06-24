bcf9b50acba469b8d0013c1dc38f8eb5b444e51e
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-06-24 19:38:40 -0300

    modifico comentarios

--- Proyecto_Integrador/controllers/commentController.js
+++ Proyecto_Integrador/controllers/commentController.js
@@ -1,44 +1,36 @@
+const { Association } = require('sequelize');
 const db = require('../database/models');
 const { validationResult } = require("express-validator");
-
+const Product = db.Product;
+const Usuario = db.Usuario;
+const Comentario = db.Comentario;
 
 const commentController = {
-
     crearComentario: function(req, res) {
         if (!req.session.user) {
             return res.redirect('/profile/login');
         }
-
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return db.Producto.findByPk(req.params.productoId, {
-                include: [{ model: db.Comentario, include: [db.Usuario] }]
+        if (errors.isEmpty()) {
+            const { comment } = req.body;
+            db.Comentario.create({
+                texto: comment,
+                usuario_id: req.session.user.id,
+                producto_id: req.params.productoId,
+                created_at: new Date()
             })
-            .then(producto => {
-                res.render('product', {
-                    producto: producto,
-                    comentarios: producto.Comentarios,
-                    errors: errors.mapped()
-                });
+           .then(() => {
+                res.redirect(`/product/${req.params.productoId}`);
+            })
+           .catch(error => {
+                res.status(500).send(error.message);
+            });
+        } else {
+            return res.render('product', {
+                errors: errors.mapped()
             });
         }
-
-        const { comment } = req.body;
-        db.Comentario.create({
-            texto: comment,
-            usuario_id: req.session.user.id,
-            producto_id: req.params.productoId,
-            created_at: new Date()
-        })
-        .then(() => {
-            res.redirect(`/product/${req.params.productoId}`);
-        })
-        .catch(error => {
-            res.status(500).send(error.message);
-        });
     },
-    
-};
-
+}
 
 module.exports = commentController;
