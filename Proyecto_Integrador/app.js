e432628fbf2a576824c66ee866fc1aaae01f9dd3
milagrosmarquez <mmarquezreymundo@udesa.edu.ar>
2024-06-24 23:28:47 -0300

    cambios

--- Proyecto_Integrador/app.js
+++ Proyecto_Integrador/app.js
@@ -16,7 +16,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middleware
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
